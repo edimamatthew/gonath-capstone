174b2f0c649e7752613f8b0523cf65fc84084e0c
Edimaobong Matthew <edimaobongmatthew@gmail.com>
2025-08-18 23:30:49 +0100

    Add main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# welcome message
+print('Hello, welcome to the world of python programming')
+
+# print my name
+print('My name is Edima')
+
+# print what i am currently doing
+print('I am currently learning python')
+
+# print an encouraging message
+print('Python is so fun to work with, come let us code together')
